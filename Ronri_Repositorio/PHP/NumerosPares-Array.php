8e940193fe351c872e2e4694b21696e321f3acc3
Rodrigo Pascual Arnaiz <ronripro@gmail.com>
2019-09-24 12:16:30 +0200

    Add files via upload

--- Ronri_Repositorio/PHP/NumerosPares-Array.php
+++ Ronri_Repositorio/PHP/NumerosPares-Array.php
@@ -1,50 +1,16 @@
 <?php
-/*
-    echo "Do while <br/>";
-    $a = 1;
-    do{
-        if( $a%2 == 0){
-            echo $a."<br/>";
-        }
-        $a ++;
-    }while($a<=100);
-    echo "while <br/>";
 
-    $c = 1;
-    while($c<=100){
-        if( $c%2 == 0){
-            echo $c."<br/>";
+    $lista = [];
+    $valor = 1;
+    while(count($lista)!=200){
+        if($valor%2 == 0){
+            $lista[] = $valor;
         }
-        $c ++;
+        $valor++;
     }
 
-    echo "For <br/>";
-    for($i = 1;$i<=100;$i++){
-        if( $i%2 == 0){
-            echo $i."<br/>";
-        }
-    }
-*/
     
-    $i = 2;
-    $cantidad = 0;
-    $time = time();
-    while(time()-$time<20){
-        $n = 2;
-        $seguir = true;
-
-        while($seguir&&$i/2>$n){
-            if($i%$n==0){
-                $seguir = false;
-            }
-            $n++;
-        }
-        if( $seguir){
-            $cantidad ++;
-        }
-
-        $i++;
+    for($i = 0;$i<count($lista);$i++){
+        echo $lista[$i]."<br/>";
     }
-    $time1 = time();
-    echo $cantidad."-Cantidad-<br/>";
 ?>
